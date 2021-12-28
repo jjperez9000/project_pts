52f28b19c902a01e6d464673ec1cb895141966f1
jjperez9000 <jjperez@clemson.edu>
2021-12-28 11:38:18 -0600

    Initial Commit

---
+++ project_pts WatchKit Extension/project_ptsApp.swift
@@ -0,0 +1,21 @@
+//
+//  project_ptsApp.swift
+//  project_pts WatchKit Extension
+//
+//  Created by John Perez on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct project_ptsApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
